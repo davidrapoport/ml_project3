593d85de33b9fa4c80d3c4a6cc1dacdcffb36741
zafarali <zafarali.ahmed@gmail.com>
2015-11-08 10:58:01 -0500

    LR_gridsearch created to run on DO

--- LR_gridsearch.py
+++ LR_gridsearch.py
@@ -19,9 +19,9 @@ lr = LogisticRegression(learning_rate=0.0005,max_iterations=100000)
 
 # In[11]:
 
-gs = grid_search.GridSearchCV(lr, {'learning_rate':[0.01, 0.001, 0.05, 0.005 ], 'max_iterations':[10, 100, 1000, 10000, 5000, 100000]}, verbose=10000)
+#gs = grid_search.GridSearchCV(lr, {'learning_rate':[0.01, 0.001, 0.05, 0.005 ], 'max_iterations':[10, 100, 1000, 10000, 5000, 100000]}, verbose=10000)
 
-# gs = grid_search.GridSearchCV(lr, {'learning_rate':[0.01, 0.001, 0.1], 'max_iterations':[10, 100, 1000, 10000, 100000]}, verbose=10000)
+gs = grid_search.GridSearchCV(lr, {'learning_rate':[0.01, 0.001, 0.1], 'max_iterations':[10, 100, 1000, 10000, 100000]}, verbose=10000, cv=3, n_jobs=7)
 
 # gs = grid_search.GridSearchCV(lr, {'learning_rate':[0.01], 'max_iterations':[10, 100]}, verbose=10000)
 
@@ -44,7 +44,7 @@ pickle.dump(gs, open('./LR_gridsearch.pickle', 'w'))
 
 # In[27]:
 
-q = pickle.load(open('./LR_gridsearch', 'r'))
+q = pickle.load(open('./LR_gridsearch.pickle', 'r'))
 
 
 # In[28]:
